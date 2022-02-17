ed1f99deca23d15db6fd423e6950b79d0f9cd708
Baaboud <Baaboud.dev@gmail.com>
2022-02-17 09:25:53 +0300

    change grammer

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const express = require("express");//return core express
-const app = express();
-const port = process.env.PORT || 5000;
+const EXPRESS = require("express");    //
+const APP = EXPRESS();
+const PORT = process.env.PORT || 5000; //Use PORT of Heroku or 5000
 
-// let server=null
-// server = app.listen(3000,function(){
-//     debug("listening on port" + app.get('port'))
-// })
-app.set("view engine", "ejs");//
-app.set('views', 'public')
+APP.set("view engine", "ejs");
+APP.set('views', 'public');
 console.log("server strtaed");
-app.use(express.static('public'));//type middelware exiet before respons //one method
-// app.use(express.static('application/css'));//type middelware exiet before respons //tow method
-app.get("/", (req, res) => {
-    var langu = ['a', 'c', 'x'];
+
+APP.use(EXPRESS.static('public'));
+APP.get("/", (req, res) => {
     res.render("index", {title: "Baaboud"});
 });
 
-
-app.listen(port);
+APP.listen(PORT);
